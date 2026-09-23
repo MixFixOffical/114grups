70e453a69422014ec4bcb74f2fd2be0e780a4ece
MixFixOffical <vasylkivmaksim@gmail.com>
2026-09-23 20:12:28 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
+// firebase-config.js (compat)
 const firebaseConfig = {
     apiKey: "AIzaSyCKMeyABL01rsQ-FcLAn81QO5TvSdxFjU",
     authDomain: "othets-1aa03.firebaseapp.com",
@@ -12,6 +9,6 @@ const firebaseConfig = {
     measurementId: "G-F50WVZRXZJ"
 };
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+firebase.initializeApp(firebaseConfig);
+window.db = firebase.firestore();
+window.auth = firebase.auth();

154333fdce4e9876b9fc17eab18a7b3c02a28297
MixFixOffical <vasylkivmaksim@gmail.com>
2026-09-23 20:15:26 +0300

    Update Firebase API key in configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // firebase-config.js (compat)
 const firebaseConfig = {
-    apiKey: "AIzaSyCKMeyABL01rsQ-FcLAn81QO5TvSdxFjU",
-    authDomain: "othets-1aa03.firebaseapp.com",
-    projectId: "othets-1aa03",
-    storageBucket: "othets-1aa03.firebasestorage.app",
-    messagingSenderId: "528392191591",
-    appId: "1:528392191591:web:1e97790e6f9f0a6e175ec2",
-    measurementId: "G-F50WVZRXZJ"
+    apiKey: "AIzaSyCKMeyABL01rsQ-FcLAn81QO5TvSdxSFjU",
+  authDomain: "othets-1aa03.firebaseapp.com",
+  projectId: "othets-1aa03",
+  storageBucket: "othets-1aa03.firebasestorage.app",
+  messagingSenderId: "528392191591",
+  appId: "1:528392191591:web:1e97790e6f9f0a6e175ec2",
+  measurementId: "G-F50WVZRXZJ"
 };
 
 firebase.initializeApp(firebaseConfig);

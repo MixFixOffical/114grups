a993f9595dd3902d545f6abe9eaa4937a2f927e9
MixFixOffical <vasylkivmaksim@gmail.com>
2026-09-23 20:06:37 +0300

    Set up Firebase configuration and exports
    Import Firebase modules and initialize Firestore and Auth.

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCKMeyABL01rsQ-FcLAn81QO5TvSdxSFjU",
     authDomain: "othets-1aa03.firebaseapp.com",
@@ -9,3 +13,9 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export { app };
+export default firebaseConfig;
